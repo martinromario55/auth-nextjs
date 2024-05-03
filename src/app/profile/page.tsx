df28789ff1b8a282e63b39948e2cdfe1c5499171
martinromario55 <martinromario55@gmail.com>
2024-05-03 08:02:00 +0300

    Middleware in NextJS

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,58 @@
-import React from 'react'
+'use client'
+import axios from 'axios'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+import toast from 'react-hot-toast'
 
 const ProfilePage = () => {
+  const router = useRouter()
+  const [data, setData] = useState('')
+
+  const logout = async () => {
+    try {
+      await axios.get('/api/users/logout')
+      toast.success('Logout successful')
+      router.push('/login')
+    } catch (error: any) {
+      console.log(error.message)
+      toast.error(error.message)
+    }
+  }
+
+  const getUserDetails = async () => {
+    const res = await axios.get('/api/users/me')
+    console.log('response:', res.data)
+    setData(res.data.data._id)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile Page</h1>
       <hr />
       <p>This is the profile page</p>
+      <h2 className="p-1 text-blue-500">
+        {data === '' ? (
+          'No data found'
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
+      <hr />
+
+      <button
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={logout}
+      >
+        Logout
+      </button>
+
+      <button
+        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={getUserDetails}
+      >
+        Fetch data
+      </button>
     </div>
   )
 }
